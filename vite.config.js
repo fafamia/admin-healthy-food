9c1c265e73f99bec8a43d9cc3f5b17ca42c7e6eb
fafamia <miapyu88@gmail.com>
2024-01-30 20:47:28 +0800

    login

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,12 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production'
+    ? '/chd104/g3/admin/'
+    : '/',
+  build: {
+    outDir: 'admin'
+  },
   plugins: [
     vue(),
   ],
