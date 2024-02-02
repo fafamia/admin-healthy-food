b66e2218290278e3c78ceb58524d0e97659e1f9c
covncovy <covncovy@gmail.com>
2024-02-02 15:40:52 +0800

    新增彈窗

--- src/views/CookbookClassView.vue
+++ src/views/CookbookClassView.vue
@@ -4,9 +4,36 @@
       <SideBar />
     </div>
     <div class="cookbook_container">
+
+      <!-- Modal -->
+      <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+        aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="staticBackdropLabel">新增資料</h5>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              <div class="mb-3">
+                <!-- 待思考如何代入商品流水號 -->
+                <h4>分類編號 0001</h4>
+                <label for="exampleFormControlInput1" class="form-label">分類名稱</label>
+                <input type="text" class="form-control" id="exampleFormControlInput1">
+              </div>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-outline-primary" data-bs-dismiss="modal">回列表</button>
+              <button type="button" class="btn btn-primary">儲存</button>
+            </div>
+          </div>
+        </div>
+      </div>
       <h1>食譜分類</h1>
       <div class="cookbook_btn">
-        <button type="button" class="btn btn-outline-primary"><i class="fa-solid fa-circle-plus"></i>新增</button>
+        <button type="button" class="btn btn-outline-primary" data-bs-toggle="modal"
+        data-bs-target="#staticBackdrop"><i
+            class="fa-solid fa-circle-plus"></i>新增</button>
       </div>
       <table class="table table-hover table-bordered border-dark">
         <thead>
