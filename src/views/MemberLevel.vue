6022d1682ae5a89c7d64a51bcba43726d452da76
fafamia <miapyu88@gmail.com>
2024-02-05 21:16:52 +0800

    修改會員專區、小遊戲、折價券

--- src/views/MemberLevel.vue
+++ src/views/MemberLevel.vue
@@ -1,89 +1,91 @@
 <template>
-    <div class="memberlevel row">
-        <SideBar class="col-3" />
-        <div class="memberlevel_right col-9">
-            <h2>會員等級</h2>
-            <div class="memberlevel_btn mb-2">
-                <button type="button" class="btn btn-outline-primary"><i class="fa-solid fa-circle-plus"></i>新增</button>
-            </div>
-            <table class="table table-hover table-bordered border-dark">
-                <tr>
-                    <th scope="col"><input type="checkbox" /></th>
-                    <th scope="col">等級編號</th>
-                    <th scope="col">等級名稱</th>
-                    <th scope="col">消費門檻</th>
-                    <th scope="col">消費折扣</th>
-                    <th scope="col"><button type="button" class="btn btn-outline-primary"><i class="fa-solid fa-trash"></i>
-                            刪除</button></th>
-                </tr>
-                <tr>
-                    <td><input type="checkbox" /></td>
-                    <td>00001</td>
-                    <td>一般會員</td>
-                    <td>0-3000</td>
-                    <td>0</td>
-                    <td><button type="button" class="btn btn-outline-primary me-2"><i
-                                class="fa-solid fa-trash"></i>刪除</button>
-                        <button type="button" class="btn btn-outline-primary"><i class="fa-solid fa-pen"></i>修改</button>
-                    </td>
-                </tr>
-                <tr>
-                    <td><input type="checkbox" /></td>
-                    <td>00002</td>
-                    <td>白金會員</td>
-                    <td>3001-8000</td>
-                    <td>0</td>
-                    <td><button type="button" class="btn btn-outline-primary me-2"><i
-                                class="fa-solid fa-trash"></i>刪除</button>
-                        <button type="button" class="btn btn-outline-primary"><i class="fa-solid fa-pen"></i>修改</button>
-                    </td>
-                </tr>
-                <tr>
-                    <td><input type="checkbox" /></td>
-                    <td>00003</td>
-                    <td>黃金會員</td>
-                    <td>8001-16000</td>
-                    <td>0.95</td>
-                    <td><button type="button" class="btn btn-outline-primary me-2"><i
-                                class="fa-solid fa-trash"></i>刪除</button>
-                        <button type="button" class="btn btn-outline-primary"><i class="fa-solid fa-pen"></i>修改</button>
-                    </td>
-                </tr>
-                <tr>
-                    <td><input type="checkbox" /></td>
-                    <td>00004</td>
-                    <td>鑽石會員</td>
-                    <td>>16000</td>
-                    <td>0.9</td>
-                    <td><button type="button" class="btn btn-outline-primary me-2"><i
-                                class="fa-solid fa-trash"></i>刪除</button>
-                        <button type="button" class="btn btn-outline-primary"><i class="fa-solid fa-pen"></i>修改</button>
-                    </td>
-                </tr>
-            </table>
-            <div class="page">
-                <nav aria-label="Page navigation example">
-                    <ul class="pagination">
-                        <li class="page-item">
-                            <a class="page-link" href="#" aria-label="Previous" style="border: none;">
-                                <span aria-hidden="true">&laquo;</span>
-                            </a>
-                        </li>
-                        <li class="page-item"><a class="page-link" href="#">1</a></li>
-                        <li class="page-item"><a class="page-link" href="#">2</a></li>
-                        <li class="page-item"><a class="page-link" href="#">3</a></li>
-                        <li class="page-item"><a class="page-link" href="#">4</a></li>
-                        <li class="page-item"><a class="page-link" href="#">5</a></li>
-                        <li class="page-item">
-                            <a class="page-link" href="#" aria-label="Next" style="border: none;">
-                                <span aria-hidden="true">&raquo;</span>
-                            </a>
-                        </li>
-                    </ul>
-                </nav>
+    <SideBar />
+    <div class="memberlevel">
+        <h2>會員等級</h2>
+        <div class="memberlevel_btn mb-2">
+            <button type="button" class="btn btn-outline-primary" data-bs-toggle="modal" data-bs-target="#addMemberLevel"><i
+                    class="fa-solid fa-circle-plus me-1"></i>新增</button>
+        </div>
+        <table class="table table-hover table-bordered border-dark">
+            <tr>
+                <th scope="col"><input type="checkbox" /></th>
+                <th scope="col">等級編號</th>
+                <th scope="col">等級名稱</th>
+                <th scope="col">消費門檻</th>
+                <th scope="col">消費折扣</th>
+                <th scope="col"><button type="button" class="btn btn-outline-primary"><i class="fa-solid fa-trash"></i>
+                        刪除</button></th>
+            </tr>
+            <tr>
+                <td><input type="checkbox" /></td>
+                <td>00001</td>
+                <td>一般會員</td>
+                <td>0-3000</td>
+                <td>0</td>
+                <td><button type="button" class="btn btn-outline-primary me-2"><i
+                            class="fa-solid fa-trash me-1"></i>刪除</button>
+                    <button type="button" class="btn btn-outline-primary"><i class="fa-solid fa-pen me-1"></i>修改</button>
+                </td>
+            </tr>
+            <tr>
+                <td><input type="checkbox" /></td>
+                <td>00002</td>
+                <td>白金會員</td>
+                <td>3001-8000</td>
+                <td>0</td>
+                <td><button type="button" class="btn btn-outline-primary me-2"><i
+                            class="fa-solid fa-trash me-1"></i>刪除</button>
+                    <button type="button" class="btn btn-outline-primary"><i class="fa-solid fa-pen me-1"></i>修改</button>
+                </td>
+            </tr>
+            <tr>
+                <td><input type="checkbox" /></td>
+                <td>00003</td>
+                <td>黃金會員</td>
+                <td>8001-16000</td>
+                <td>0.95</td>
+                <td><button type="button" class="btn btn-outline-primary me-2"><i
+                            class="fa-solid fa-trash me-1"></i>刪除</button>
+                    <button type="button" class="btn btn-outline-primary"><i class="fa-solid fa-pen me-1"></i>修改</button>
+                </td>
+            </tr>
+            <tr>
+                <td><input type="checkbox" /></td>
+                <td>00004</td>
+                <td>鑽石會員</td>
+                <td>>16000</td>
+                <td>0.9</td>
+                <td><button type="button" class="btn btn-outline-primary me-2"><i
+                            class="fa-solid fa-trash me-1"></i>刪除</button>
+                    <button type="button" class="btn btn-outline-primary"><i class="fa-solid fa-pen me-1"></i>修改</button>
+                </td>
+            </tr>
+        </table>
+        <div class="modal fade" id="addMemberLevel" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">新增會員等級</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body text-start">
+                        <p>等級編號: 00005</p>
+                        <label for="level_name" class="mb-3 me-2">等級名稱</label>
+                        <input type="text" name="level_name" id="" class="w-75">
+                        <div class="level_pay mb-3">
+                            <label for="level_num" class=" me-2">消費門檻</label>
+                            <input type="number" name="level_num" id="level_num"> 至 <input type="number"
+                                name="level_num_end" id="">
+                        </div>
+                        <label for="level_name" class="mb-3 me-2">消費折扣</label>
+                        <input type="text" name="level_name" id="" class="w-75">
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-primary">確定</button>
+                    </div>
+                </div>
             </div>
         </div>
-
     </div>
 </template>
 <script>
@@ -101,5 +103,4 @@ export default {
 
 <style lang="scss">
 @import "@/assets/scss/page/memberlevel.scss";
-@import "@/assets/scss/sidebar.scss";
-</style>
+@import "@/assets/scss/sidebar.scss";</style>
--- src/views/MemberView.vue
+++ src/views/MemberView.vue
@@ -1,12 +1,12 @@
 <template>
-    <div class="member row">
-        <SideBar class="col-3" />
-        <div class="member_right col-9">
-            <h2>會員管理</h2>
-            <div class="member_search_area mb-2">
-                <input type="text" class="member_search primary" value="" placeholder="搜尋">
-            </div>
-            <table class="table table-hover table-bordered border-dark">
+    <SideBar />
+    <div class="member">
+        <h2>會員管理</h2>
+        <div class="member_search_area mb-2">
+            <input type="text" class="member_search primary" value="" placeholder="搜尋">
+        </div>
+        <table class="table table-hover table-bordered border-dark">
+            <thead>
                 <tr>
                     <th scope="col">會員編號</th>
                     <th scope="col">姓名</th>
@@ -15,63 +15,69 @@
                     <th scope="col">狀態</th>
                     <th scope="col">註冊時間</th>
                 </tr>
-                <!-- 使用v-for的資料吃不到BS樣式 -->
-                <!-- <tr v-for="person in members" :key="person.id">
-                    <td>{{ person.id }}</td>
+            </thead>
+            <tbody>
+                <tr v-for="person in members" :key="person.id">
+                    <td><a type="button" class="member-no" data-bs-toggle="modal" data-bs-target="#memberInfo">
+                            {{ person.id }}
+                        </a></td>
                     <td>{{ person.name }}</td>
                     <td>{{ person.email }}</td>
                     <td>{{ person.phone }}</td>
                     <td>{{ person.status }}</td>
                     <td>{{ person.date }}</td>
-                </tr> -->
-                <tr>
-                    <td>00001</td>
-                    <td>王小明</td>
-                    <td>aabbcc@test.com</td>
-                    <td>0988123456</td>
-                    <td>正常</td>
-                    <td>2023/12/30 </td>
                 </tr>
-                <tr>
-                    <td>00001</td>
-                    <td>王小明</td>
-                    <td>aabbcc@test.com</td>
-                    <td>0988123456</td>
-                    <td>正常</td>
-                    <td>2023/12/30 </td>
-                </tr>
-                <tr>
-                    <td>00001</td>
-                    <td>王小明</td>
-                    <td>aabbcc@test.com</td>
-                    <td>0988123456</td>
-                    <td>正常</td>
-                    <td>2023/12/30 </td>
-                </tr>
-            </table>
-            <div class="page">
-                <nav aria-label="Page navigation example">
-                    <ul class="pagination">
-                        <li class="page-item">
-                            <a class="page-link" href="#" aria-label="Previous" style="border: none;">
-                                <span aria-hidden="true">&laquo;</span>
-                            </a>
-                        </li>
-                        <li class="page-item"><a class="page-link" href="#">1</a></li>
-                        <li class="page-item"><a class="page-link" href="#">2</a></li>
-                        <li class="page-item"><a class="page-link" href="#">3</a></li>
-                        <li class="page-item"><a class="page-link" href="#">4</a></li>
-                        <li class="page-item"><a class="page-link" href="#">5</a></li>
-                        <li class="page-item">
-                            <a class="page-link" href="#" aria-label="Next" style="border: none;">
-                                <span aria-hidden="true">&raquo;</span>
-                            </a>
-                        </li>
-                    </ul>
-                </nav>
+            </tbody>
+        </table>
+        <div class="modal fade" id="memberInfo" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body text-start">
+                        <p>編號: 00001</p>
+                        <p>姓名: 王小明</p>
+                        <p>EMAIL: aabbcc@test.com</p>
+                        <p>電話: 0988-123456</p>
+                        <p>生日: 1988-08-08</p>
+                        <p>地址: 桃園市中壢區成功路888號</p>
+                        <p>累計消費金額: 1000</p>
+                        <p>註冊時間: 2023/12/30</p>
+                        <p>會員等級: 一般會員</p>
+                        <p>狀態: <select id="member_status">
+                                <option value="正常">正常</option>
+                                <option value="黑名單">黑名單</option>
+                            </select></p>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">回列表</button>
+                        <button type="button" class="btn btn-primary">儲存</button>
+                    </div>
+                </div>
             </div>
         </div>
-
+        <div class="page">
+            <nav aria-label="Page navigation example">
+                <ul class="pagination">
+                    <li class="page-item">
+                        <a class="page-link" href="#" aria-label="Previous" style="border: none;">
+                            <span aria-hidden="true">&laquo;</span>
+                        </a>
+                    </li>
+                    <li class="page-item"><a class="page-link" href="#">1</a></li>
+                    <li class="page-item"><a class="page-link" href="#">2</a></li>
+                    <li class="page-item"><a class="page-link" href="#">3</a></li>
+                    <li class="page-item"><a class="page-link" href="#">4</a></li>
+                    <li class="page-item"><a class="page-link" href="#">5</a></li>
+                    <li class="page-item">
+                        <a class="page-link" href="#" aria-label="Next" style="border: none;">
+                            <span aria-hidden="true">&raquo;</span>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
     </div>
 </template>
 
@@ -88,7 +94,23 @@ export default {
                     phone: '0988123456',
                     status: '正常',
                     date: '2023/12/30'
-                }
+                },
+                {
+                    id: '00002',
+                    name: '王小明',
+                    email: 'aabbcc@test.com',
+                    phone: '0988123456',
+                    status: '正常',
+                    date: '2023/12/30'
+                },
+                {
+                    id: '00003',
+                    name: '王小明',
+                    email: 'aabbcc@test.com',
+                    phone: '0988123456',
+                    status: '正常',
+                    date: '2023/12/30'
+                },
             ]
         }
     },
